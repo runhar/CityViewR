485b8f3bdbac2e8267e6d5f3884977a02e1101e6
runhar <runhar04@gmail.com>
2017-01-18 23:52:31 +0100

    Added default local dataset, set bars according to data

--- cityviewr.js
+++ cityviewr.js
@@ -1,6 +1,11 @@
 var mapEl = document.querySelector('a-map');
 var currentLocationEl = document.querySelector('#current-location');
 var setProperty = window.AFRAME.utils.entity.setComponentProperty;
+var queryData = ['Marokko_19','NederlandseAntillenEnAruba_20','Suriname_21','Turkije_22','OverigNietWesters_23'];
+
+
+//http://opendata.cbs.nl/ODataApi/odata/83220NED/TypedDataSet?$filter=(substringof('WK0363',WijkenEnBuurten))&$select=WijkenEnBuurten,Marokko_19,NederlandseAntillenEnAruba_20,Suriname_21,Turkije_22,OverigNietWesters_23
+//max = Object.keys(obj).reduce(function(m, k){ return obj[k] > m ? obj[k] : m }, -Infinity);
 
 //helper function to find index of item in array
 function findWithAttr(array, attr, value) {
@@ -28,7 +33,9 @@ function addMarker(position, id) {
     point.setAttribute('position', position);
     point.setAttribute('color', 'lime');
     point.setAttribute('class', 'bar');
-    point.setAttribute('bar', id + 0);
+    point.setAttribute('bar', id);
+    point.setAttribute('id', id + 0);
+    point.setAttribute('visible', false);
     marker.appendChild(point);
     mapEl.appendChild(marker);
     return marker;
@@ -81,10 +88,7 @@ function addVarButton(vartext, classname, id, count) {
 
 
 function get_CBS_varnames() {
-    //Get data from CBS Open Data
-    //Creating a menu array of dicts, containing
-    //id: level, title, key, type, items (childitems)
-    //With this array build a menu
+    //Get data categories from CBS Open Data
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (xhr.readyState == XMLHttpRequest.DONE) {
@@ -95,6 +99,7 @@ function get_CBS_varnames() {
             for (var i = 0, len = variables.length; i < len; i++) {
                 menuLookup[variables[i].ID] = variables[i];
             }
+            //create array where categories have children instead of a parent
             variables = variables.reduce(function(map, node) {
                 map.i[node.ID] = node;
                 node.children = [];
@@ -106,7 +111,7 @@ function get_CBS_varnames() {
                 i: {},
                 result: []
             }).result;
-
+            //remove some unnecessary items from menuData
             var remove = ['Wijken en buurten', 'Regioaanduiding', 'Postcode'];
             for (var i = 0; i < remove.length; i++) {
                 var index = findWithAttr(variables, 'Title', remove[i]);
@@ -121,7 +126,7 @@ function get_CBS_varnames() {
             CBSdata.children = variables;
             menuData[0] = CBSdata;
             console.log(menuData);
-            //remove some unnecessary items from menuData
+
             button = addVarButton(menuData[0].Title, 'CBStopcat', menuData[0].ID, 0);
             menuLookup[999] = menuData[0];
             for (var i = 0; i < menuData[0]['children'].length; i++) {
@@ -132,7 +137,7 @@ function get_CBS_varnames() {
         }
 
     }
-    xhr.open('GET', 'http://opendata.cbs.nl/ODataApi/odata/83487NED/DataProperties', true);
+    xhr.open('GET', 'http://opendata.cbs.nl/ODataApi/odata/83220NED/DataProperties', true);
     xhr.send(null);
 }
 
@@ -155,7 +160,7 @@ function onLocationUpdate(lat, long, width, height) {
             district.marker = addMarker({
                 x: 0,
                 y: 0,
-                z: 0.15
+                z: 0
             }, district.id);
             districtCache[district.id] = district;
         }
@@ -172,21 +177,66 @@ function onLocationUpdate(lat, long, width, height) {
             } else {
                 setProperty(district.marker, 'visible', true);
             }
-            setProperty(district.marker, 'id', district.id);
+            //setProperty(district.marker, 'id', district.id+0);
             setProperty(district.marker, 'position', position);
             extra_markers = create_extra_markers(position, district.id);
         });
+        //load default data
+        loadJSON('data/default_data.json', function(response) {
+            var data = JSON.parse(response);
+            var max = Object.keys(data).reduce(function(m, k){ return data[k] > m ? data[k] : m }, -Infinity);
+            barsSet = setBars(data.value, max);
+          });
     });
 }
 
+function setBars(data, max){
+    dataLookup = {};
+    for (var i = 0, len = data.length; i < len; i++) {
+        dataLookup[data[i]['WijkenEnBuurten'].trim()] = data[i];
+    }
+  console.log(dataLookup);
+  nrOfItems = Object.keys(data[0]).length;
+  var barColors = chroma.scale('YlOrBr').colors(nrOfItems-1);
+  var vals = Object.keys(data).map(function(key) {
+		return data[key];
+	});
+  var heights = [];
+  var barData = [];
+  for (var i=0; i<data.length;i++) {
+		var wk = vals[i]['WijkenEnBuurten'];
+		for (var j=1; j<nrOfItems;j++) {
+      var barNr = j-1;
+			var val = vals[i][queryData[barNr]];
+      var color = barColors[barNr];
+			heights.push(val);
+			barData.push({"district":wk.trim()+barNr,"value":val, 'color':color});
+		}
+	}
+
+	var maxBarValue = Math.max.apply(Math, heights);
+  var allBars = document.getElementsByClassName('bar');
+  for (var i=0; i<barData.length;i++){
+    var bar = document.getElementById(barData[i]['district']);
+    var height = barData[i]['value'];
+    var position = new THREE.Vector3();
+		position.setFromMatrixPosition(bar.object3D.matrixWorld);
+    //position.y = (height/maxBarValue)/2;
+    //bar.setAttribute('position', {position[x], position[y], z});
+    //bar.object3D.position.set(position.x, position.y, position.z);
+    bar.setAttribute('height', (height/maxBarValue));
+    bar.setAttribute('color', barData[i]['color']);
+    bar.setAttribute('visible', true);
+  }
+}
+
 function create_extra_markers(position, id) {
-    for (var i = 0; i < 3; i++) {
+    for (var i = 0; i < 4; i++) {
         var barNr = i + 1;
         var posx = position.x + 0.03 + i * 0.03;
         var posy = position.y;
-        var posz = 0.1;
+        var posz = 0;
         var extra_markers = [];
-        console.log(position.x);
         var marker = document.createElement('a-entity');
         var point = document.createElement('a-box');
         point.setAttribute('height', 0.2);
@@ -204,8 +254,9 @@ function create_extra_markers(position, id) {
         });
         point.setAttribute('color', 'fuchsia');
         point.setAttribute('class', 'bar');
-        point.setAttribute('bar', id + barNr);
-        point.setAttribute('isFirst', false);
+        point.setAttribute('bar', id);
+        point.setAttribute('id', id + barNr);
+        point.setAttribute('visible', false);
         marker.appendChild(point);
         mapEl.appendChild(marker);
         extra_markers.push(marker);
@@ -213,11 +264,24 @@ function create_extra_markers(position, id) {
     return extra_markers;
 }
 
+function addViveControls(){
+  var scene = document.getElementById("scene");
+  var leftcontrol = document.createElement('a-entity');
+  var rightcontrol = document.createElement('a-entity');
+  leftcontrol.setAttribute('vive-controls',"hand: left");
+  rightcontrol.setAttribute('vive-controls',"hand: right");
+  scene.appendChild(leftcontrol);
+  scene.appendChild(rightcontrol);
+  var cursor = document.getElementById('cursor');
+  cursor.setAttribute('fuse', false);
+}
 //------------START------------------------------------------------------------
 
+if (AFRAME.utils.checkHeadsetConnected()){
+  addViveControls();
+}
 var menu = get_CBS_varnames();
 
-//get_CBS_varnames_local();
 
 // Once the map is loaded
 
--- start_server.bat
+++ start_server.bat
@@ -1,1 +1,2 @@
 python -m SimpleHTTPServer
+start C:\Users\harol\Documents\Thuis\3D\Chrome-bin\chrome.exe http://127.0.0.1:8000/
